73adb177c0f1de040684499f0b48afa034534a4c
Natayudha <rioganatayudha0103@mail.ugm.ac.id>
2023-12-06 11:05:55 +0700

    revisi

--- routes/api.php
+++ routes/api.php
@@ -6,21 +6,24 @@ use App\Http\Controllers\InfoController;
 use App\Http\Controllers\GreetController;
 
 
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-
 });
+
+Route::get('/api-docs.json', 'GreetController@apiDocs');
+
 Route::get('/info', [InfoController::class, 'index'])->name('info');
 Route::get('/greet', [GreetController::class,'greet'])->name('greet');
+Route::get('/getPicture', [GreetController::class,'getPicture'])->name('apiGetPicture');
+Route::post('/postPicture', [GreetController::class,'storePicture'])->name('apiPostPicture');
